2d93423c87177824669eb2df6fbf48d562c685f2
zwf193071 <997131679@qq.com>
2020-05-11 17:12:10 +0800

    connect to mongodb successfully

--- README.md
+++ README.md
@@ -7,9 +7,10 @@
 安装该包，无需重启服务，刷新页面即可看到最新效果
 
 ## GraphQL
+GraphQL是比REST更高效、强大和灵活的新一代API标准。Facebook开发了GraphQL并且将其开源，目前其由一大群来自全球各地的公司和个人维护。
+* 注意：GraphQL是API标准，并不是一种数据库技术。
 
-
-## Usage
+## 
 
 
 
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,12 +1,8 @@
-let notes = [
-    { id: '1', content: 'first one', author: 'lucy' },
-    { id: '2', content: 'second one', author: 'bruce' },
-    { id: '2', content: 'third one', author: 'jackie' }
-]
 module.exports = {
-    hello: () => 'Hello my lover!',
-    notes: () => notes,
-    note: (parent, args) => {
-        return notes.find(note => note.id === args.id);
+    notes: async (parent, args, { models }) => {
+        return await models.Note.find();
+    },
+    note: async (parent, args, { models }) => {
+        return await models.Note.findById(args.id);
     }
 }
