9b4131520c0f839e0b484e83ea31809af349baed
zhuwenfang <zhuwenfang@inspur.com>
2020-05-14 15:46:22 +0800

    add nest queries

--- src/resolvers/note.js
+++ src/resolvers/note.js
@@ -2,5 +2,8 @@ module.exports = {
   // Resolve the author info for a note when requested
   author: async (note, args, { models }) => {
     return await models.User.findById(note.author);
+  },
+  favoritedBy: async (note, args, { models }) => {
+    return await models.User.find({ _id: {$in: note.favoritedBy }});
   }
 };
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -5,10 +5,13 @@ module.exports = {
     note: async (parent, args, { models }) => {
         return await models.Note.findById(args.id);
     },
-    // user: async (parent, args, { models }) => {
-    //     return await models.User.findOne({ username: args.username });
-    // },
-    // users: async (parent, args, { models }) => {
-    //     return await models.User.find({}).limit(100);
-    // }
+    user: async (parent, { username }, { models }) => {
+        return await models.User.findOne({ username });
+    },
+    users: async (parent, args, { models }) => {
+        return await models.User.find({});
+    },
+    me: async(parent, args, { models, user }) => {
+        return await models.User.findById(user.id);
+    }
 }
